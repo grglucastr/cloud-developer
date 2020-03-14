49fe703dfad3f6da8b54256bba27452b630f8e93
George Bentes <george.bentes@gmail.com>
2020-03-13 22:01:53 -0300

    Add patch implementation

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -34,8 +34,23 @@ router.get('/:id', async(req:Request, res:Response) => {
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const { id } = req.params;
+        if(!id){
+            throw new Error('Id param is required');
+        }
+
+        const { body } = req;
+
+        FeedItem.findByPk(id).then((feedItem) => {
+            if(feedItem === null){
+                return res.status(404).send([]);
+            }
+
+            const newValues = {...feedItem.dataValues, ...body};
+            feedItem.update(newValues, {fields: Object.keys(body)}).then((updated) => {
+                return res.status(200).send(updated);
+            });            
+        });
 });
 
 
